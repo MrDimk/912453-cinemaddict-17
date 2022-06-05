33040ba322302ed6253f3353cfa4e91e691cc8a4
Dmitriy Kozlov <mr.dimk@icloud.com>
2022-06-05 22:42:20 +0300

    Реализует сортировку

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import FilmsPresenter from './presenter/films-presenter';
 import DataModel from './model/model';
 import ProfilePresenter from './presenter/profile-presenter';
 import FilterPresenter from './presenter/filter-presenter';
-import SortPresenter from './presenter/sort-presenter';
 import FooterPresenter from './presenter/footer-presenter';
 
 const headerElement = document.querySelector('.header');
@@ -12,14 +11,12 @@ const dataModel = new DataModel();
 const data = dataModel.loadData();
 const profileData = dataModel.loadUserData();
 
-const filmsPresenter = new FilmsPresenter();
+const filmsPresenter = new FilmsPresenter(mainElement);
 const profilePresenter = new ProfilePresenter(profileData);
 const filterPresenter = new FilterPresenter(data);
-const sortPresenter = new SortPresenter();
 const footerPresenter = new FooterPresenter(data);
 
 profilePresenter.init(headerElement);
+filmsPresenter.init(data);
 filterPresenter.init(mainElement);
-sortPresenter.init(mainElement);
 footerPresenter.init(footerStatisticsElement);
-filmsPresenter.init(mainElement, data);
